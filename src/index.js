02d0a7495dffa67b90194ac1e94684f9e6a0cca8
Rujin Shrestha <63194470+TheNyachhon@users.noreply.github.com>
2021-06-07 15:46:37 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
+console.log("hello");
